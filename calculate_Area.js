892fb3dd4d01ea3de89f465a4286c9ba0fe203ad
QuestScrolls <mixiyomi@gamil.com>
2025-09-05 09:14:29 -0400

    Area Calculator

---
+++ calculate_Area.js
@@ -0,0 +1,12 @@
+let width;
+let lenght;
+
+function calculateArea() {
+    length = parseFloat(document.getElementById('lenght').value);
+    width = parseFloat(document.getElementById('width').value);
+    
+    let area = length * width;
+
+    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+
+}
